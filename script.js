c4709d23600e07c9e1ecc5a1920e7aac549d0b52
ShivamP-97 <shivampathak19972@gmail.com>
2026-02-05 22:09:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+
+let signup_form = document.getElementById("signup-form");
+let name = document.getElementById("name");
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+let confirm_password = document.getElementById("confirm-password");
+
+
+signup_form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let pass = password.value.trim();
+    let pass2 = confirm_password.value.trim();
+
+    if (pass === pass2) {
+        sessionStorage.setItem("name", name.value.trim());
+        sessionStorage.setItem("email", email.value.trim());
+        sessionStorage.setItem("password", pass);
+
+        alert("Sign up successful!");
+		signup_form.reset();
+    } else {
+        alert("Passwords do not match");
+    }
+});
